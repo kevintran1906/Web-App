e409ac95ffc2f0ddbc05f65b164b281a9270639c
Kevin Tran <kevintran19062007@gmail.com>
2024-12-05 10:14:14 -0500

    add image

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Earth Main Page" src="./assets/earth.png">
+  <HelloWorld msg="Let us take care of your trip"/>
 </template>
 
 <script>
